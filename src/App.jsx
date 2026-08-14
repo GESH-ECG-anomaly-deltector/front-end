b4c51e74714cc5b09c30c988ee3282dbedc06e20
Zeinab-Jannati <zeynabjannati5@gmail.com>
2026-08-14 12:14:29 +0330

    feat(app.jsx): add some routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import { Routes, Route} from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import './App.css'
 
 function App() {
   return (
-    <Home />
+    <Routes>
+      <Route path='/' element={ <Home /> }/>
+      {/* <Route path='/login-signup' element={  }/>
+      <Route path='/doctor-panel' element={  }/>
+      <Route path='/patient-panel' element={  }/>
+      <Route path='/admin-panel' element={  }/> */}
+    </Routes>
   )
 }
 
