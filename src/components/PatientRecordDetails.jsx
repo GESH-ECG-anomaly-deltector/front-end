01687d8423600d31883e4b2ac415b71c58dc6ce1
Zeinab-Jannati <zeynabjannati5@gmail.com>
2026-08-16 11:27:12 +0330

    fix(routing): fix broken link path to patient record details page

--- src/components/PatientRecordDetails.jsx
+++ src/components/PatientRecordDetails.jsx
@@ -1,7 +1,14 @@
+import { useParams } from 'react-router-dom';
+
+import PatientHeader from './PatientHeader'
+
 const PatientRecordDetails = () => {
+    const { recordId } = useParams();
+
     return ( 
-        <>
-        </>
+        <div>
+            <h1>نتجه REC - { recordId }</h1>
+        </div>
      );
 }
  
